e9b82057c37ed74540f36e104a0db9c17337a094
mdwajidtechs <wajidtech.coms@gmail.com>
2025-02-23 07:47:49 +0530

    tewst

---
+++ static/cx/pscript2.js
@@ -0,0 +1,19 @@
+const productContainers = [...document.querySelectorAll('.product-container2')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    console.log(item);
+    let containerDimensions = item.getBoundingClientRect();
+    console(containerDimensions)
+    console.log("make your world");
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
